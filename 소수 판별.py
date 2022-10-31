853680ccd592d53ec038d815bd2f2f879d63d349
seungchan2022 <110214307+seungchan2022@users.noreply.github.com>
2022-10-31 13:29:17 +0900

    Add files via upload

---
+++ 소수 판별.py
@@ -0,0 +1,39 @@
+# 소수 판별
+
+import math
+
+# 소수 판별 함수(2이상의 자연수에 대해)
+def prime(x):
+    # 2부터 x의 제곱근까지의 모든 수를 확인 하며
+    for i in range(2, int(math.sqrt(x)) + 1):
+        # x가 해당 수로 나누어 떨어진다면
+        if x % i == 0:
+            return False    # 소수 X
+    return True
+
+print(prime(5))
+print(prime(8))
+
+
+# 에라토스테네스의 체 알고리즘
+
+import math
+
+n = 1000    # 2 ~ 1,000까지의 모든 수에 대해 소수 판별
+# 처음엔 모든 수가 소수(True)인 것으로 초기화(0과 1은 제외)
+array = [True for i in range(n + 1)]
+
+# 에라토스체네스의 체 알고리즘 수행
+# 2부터 n의 제곱근까지의 모든 수를 확인하며
+for i in range(2, int(math.sqrt(n)) + 1):
+    if array[i] == True:    # i가 소수인 경우(남은 수인 경우)
+        # i를 제외한 i의 모든 배수 지우기
+        j = 2   # 배수
+        while i * j <= n:
+            array[i * j] = False
+            j += 1  # 배수 늘리기
+
+# 모든 소수 출력
+for i in range(2, n + 1):
+    if array[i]:
+        print(i, end=' ')
